83c3495c7c16a177f80ae80940452df95eb6963d
Tabish BarYar <tabishaliarqam.347@gmail.com>
2024-11-06 15:14:14 +0500

    added webClient

---
+++ webflux/src/main/java/Webflux/client/UserClientController.java
@@ -0,0 +1,40 @@
+package Webflux.client;
+
+import Webflux.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/client/users")
+@RequiredArgsConstructor
+public class UserClientController {
+
+    private final UserClientService userClientService;
+
+    @GetMapping
+    public Flux<User> getAllUsers() {
+        return userClientService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<User> getUserById(@PathVariable Long id) {
+        return userClientService.getUserById(id);
+    }
+
+    @PostMapping
+    public Mono<User> createUser(@RequestBody User user) {
+        return userClientService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public Mono<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userClientService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> deleteUser(@PathVariable Long id) {
+        return userClientService.deleteUser(id);
+    }
+}
